7a50f4777c83660c141ed56307d7ec40fdcc7003
Paweł Mudlaff <mudlaff.p@gmail.com>
2017-04-26 21:38:07 +0200

    Generate project from cookie cuter. Add two views to serv html and json response

---
+++ pyramid_hs/routes.py
@@ -0,0 +1,5 @@
+def includeme(config):
+    config.add_static_view('static', 'static', cache_max_age=3600)
+    config.add_route('home', '/')
+    config.add_route('hello_json', '/hello_j/', request_method='GET')
+    config.add_route('hello_html', '/hello_h/', request_method='GET')
